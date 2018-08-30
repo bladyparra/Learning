0c14d2a543f84be556f584d479f2edee9345bd58
Bladimir Gerson Parra Bermudez <ipproyectossoluciones@gmail.com>
2018-08-30 16:16:12 -0500

    Se crea envío de correo al profesor cuando se aprueba o rechaza un curso con VueTables 2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Course;
-// use App\Mail\CourseApproved;
-// use App\Mail\CourseRejected;
+use App\Mail\CourseApproved;
+use App\Mail\CourseRejected;
 use App\VueTables\EloquentVueTables;
 use Illuminate\Http\Request;
 
@@ -24,4 +24,44 @@ class AdminController extends Controller
         }
         return abort (401);
     }
+
+    public function updateCourseStatus () 
+    {
+		if (\request()->ajax()) {
+			$course = Course::find(\request('courseId'));
+
+			if(
+				(int) $course->status !== Course::PUBLISHED &&
+				! $course->previous_approved &&
+				\request('status') === Course::PUBLISHED
+			) {
+				$course->previous_approved = true;
+				\Mail::to($course->teacher->user)->send(new CourseApproved($course));
+			}
+
+			if(
+				(int) $course->status !== Course::REJECTED &&
+				! $course->previous_rejected &&
+				\request('status') === Course::REJECTED
+			) {
+				$course->previous_rejected = true;
+				\Mail::to($course->teacher->user)->send(new CourseRejected($course));
+			}
+
+			$course->status = \request('status');
+			$course->save();
+			return response()->json(['msg' => 'ok']);
+		}
+		return abort(401);
+	}
+
+    public function students () 
+    {
+		return view('admin.students');
+	}
+
+    public function teachers () 
+    {
+		return view('admin.teachers');
+	}
 }
